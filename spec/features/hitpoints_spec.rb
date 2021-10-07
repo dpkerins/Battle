ec7b2819d1a3b76424a1cc806c201d57cb2064c0
Douglas <douglaskerins@gmail.com>
2021-10-07 12:44:08 +0100

    Add attack and receive_damage methods, testing, refactoring

--- spec/features/hitpoints_spec.rb
+++ spec/features/hitpoints_spec.rb
@@ -1,6 +1,6 @@
 feature "View hit points" do
   scenario 'see player 2 hit poiints' do
     sign_in_and_play
-    expect(page).to have_content 'Chris: 50HP'
+    expect(page).to have_content 'Chris: 60HP'
   end
 end
