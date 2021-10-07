6f84d8ebd364ded0661a27cdc2b39bba697d989d
Douglas <douglaskerins@gmail.com>
2021-10-07 12:19:17 +0100

    Adding model layer

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-require "sinatra/reloader"
+require 'sinatra/reloader'
 
 class Battle < Sinatra::Base
   configure :development do
@@ -13,20 +13,20 @@ class Battle < Sinatra::Base
   end
   
   post '/names' do
-    session[:player1] = params[:player1]
-    session[:player2] = params[:player2]
+    $player1 = Player.new(params[:player1])
+    $player2 = Player.new(params[:player2])
     redirect 'play'
   end
 
   get '/play' do
-    @player1 = session[:player1]
-    @player2 = session[:player2]
+    @player1_name = $player1.name
+    @player2_name = $player2.name
     erb(:play)
   end
 
   get'/attack' do
-    @player1 = session[:player1]
-    @player2 = session[:player2]
+    @player1_name = $player1.name
+    @player2_name = $player2.name
     erb(:attack)
   end
 
